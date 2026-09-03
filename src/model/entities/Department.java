4575c4041e9381e447bebb467bfaff181d748f66
tiagooayala <tiagooayala@gmail.com>
2026-09-03 17:14:26 -0300

    Implementando classe Seller

--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -1,9 +1,13 @@
 package model.entities;
 
+import java.io.Serial;
 import java.io.Serializable;
 import java.util.Objects;
 
 public class Department implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
 
     private Integer id;
     private String name;
